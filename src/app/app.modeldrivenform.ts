525717e0f67632b76e907f68d586e4baaade484b
tushar_scaledesk <tushar@scaledesk.com>
2016-12-30 05:28:34 +0530

    add components

---
+++ src/app/app.modeldrivenform.ts
@@ -0,0 +1,18 @@
+/**
+* @Author: Tushar Agarwal(tusharcoder) <tushar>
+* @Date:   2016-12-30T05:25:50+05:30
+* @Email:  tamyworld@gmail.com
+* @Filename: app.modeldrivenform.ts
+* @Last modified by:   tushar
+* @Last modified time: 2016-12-30T05:27:24+05:30
+*/
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'template-driven',
+  templateUrl:`./app.modeldrivenform.html`
+})
+
+export class ModelDrivenForm{
+
+}
---
+++ src/app/app.templatedrivenform.ts
@@ -0,0 +1,18 @@
+/**
+* @Author: Tushar Agarwal(tusharcoder) <tushar>
+* @Date:   2016-12-30T05:22:22+05:30
+* @Email:  tamyworld@gmail.com
+* @Filename: app.templatedrivenform.ts
+* @Last modified by:   tushar
+* @Last modified time: 2016-12-30T05:25:15+05:30
+*/
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'template-driven',
+  template:`<h1>This is the template driven form</h1>`
+})
+
+export class TemplateDrivenForm{
+
+}

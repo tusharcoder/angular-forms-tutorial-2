5f7e7b375aa26db963b867a0acea865480bda080
tushar_scaledesk <tushar@scaledesk.com>
2016-12-30 11:24:34 +0530

    add error classes to the controls on error

--- src/app/app.formvalidations.ts
+++ src/app/app.formvalidations.ts
@@ -4,7 +4,7 @@
 * @Email:  tamyworld@gmail.com
 * @Filename: app.formvalidations.ts
 * @Last modified by:   tushar
-* @Last modified time: 2016-12-30T11:14:58+05:30
+* @Last modified time: 2016-12-30T11:24:02+05:30
 */
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, FormControl,Validators} from '@angular/forms';
@@ -17,36 +17,42 @@ import {FormBuilder, FormGroup, FormControl,Validators} from '@angular/forms';
     </div>
     <div class="jumbotron">
       <form [formGroup]="modelDrivenForm" (ngSubmit)='submitForm(modelDrivenForm.value)' >
-        <div class="form-group">
+        <div class="form-group" [ngClass]="{'has-error':!modelDrivenForm.controls['email'].valid&&modelDrivenForm.controls['email'].touched}">
           <label for="emailInput1">Email:</label>
           <input type="text" name="email" class="form-control" id="emailInput1" placeholder="user@somewhere.com" [formControl]="modelDrivenForm.controls['email']">
         </div>
         <div class="alert alert-danger" *ngIf="modelDrivenForm.controls['email'].hasError('required')&&modelDrivenForm.controls['email'].touched">
         *Email is required
         </div>
-        <div class="alert alert-danger" *ngIf="modelDrivenForm.controls['email'].hasError('validateEmail')&&modelDrivenForm.controls['email'].touched">
+        <div class="alert alert-danger" *ngIf="!modelDrivenForm.controls['email'].hasError('required')&&modelDrivenForm.controls['email'].hasError('validateEmail')&&modelDrivenForm.controls['email'].touched">
         *Valid Email is required
         </div>
-        <div class="form-group">
+        <div class="form-group" [ngClass]="{'has-error':!modelDrivenForm.controls['password'].valid&&modelDrivenForm.controls['password'].touched}">
           <label for="passwordInput1">Password:</label>
           <input type="password" name="password" class="form-control" id="passwordInput1" placeholder="password" [formControl]="modelDrivenForm.controls['password']">
         </div>
 
-        <div class="alert alert-danger">
+        <div class="alert alert-danger" *ngIf="modelDrivenForm.controls['password'].hasError('required')&&modelDrivenForm.controls['password'].touched">
         *Password is required
         </div>
-        <div class="form-group">
+        <div class="alert alert-danger" *ngIf="modelDrivenForm.controls['password'].hasError('minlength')&&modelDrivenForm.controls['password'].touched">
+        *Password must be of atleast 5 charactors
+        </div>
+        <div class="alert alert-danger" *ngIf="modelDrivenForm.controls['password'].hasError('maxlength')&&modelDrivenForm.controls['password'].touched">
+        *Password must not greator than 10 charactors
+        </div>
+        <div class="form-group" [ngClass]="{'has-error':!modelDrivenForm.controls['dob'].valid&&modelDrivenForm.controls['dob'].touched}">
           <label for="dateInput1"></label>
           <input type="date" name="dob" class="form-control" id="dateInput1" placeholder="Date of Birth:" [formControl]="modelDrivenForm.controls['dob']">
         </div>
 
-        <div class="alert alert-danger">
-        *Date of Birth is required is required
+        <div class="alert alert-danger" *ngIf="modelDrivenForm.controls['dob'].hasError('required')&&modelDrivenForm.controls['dob'].touched">
+        *Date of Birth is required
         </div>
         <div class="form-group">
           <label>Gender</label>
 
-          <div class="alert alert-danger">
+          <div class="alert alert-danger" *ngIf="modelDrivenForm.controls['gender'].hasError('required')&&modelDrivenForm.controls['gender'].touched">
           *Please select your Gender
           </div>
           <div class="radio">
